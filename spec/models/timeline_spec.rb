1f2a3ba6caaec40c27ac6ae39d75c4f146a0ff7d
mfigand <m.figand@gmail.com>
2020-03-25 19:33:06 +0100

    Add timeline specs

--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -3,5 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe Timeline, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:timeline) { build(:timeline) }
+
+  describe 'validations' do
+    it 'Create a valid timeline' do
+      expect(create(:timeline)).to be_valid
+    end
+
+    it 'Validwithout protagonist' do
+      expect(build(:timeline, protagonist: nil)).to be_valid
+    end
+
+    it 'Invalidwithout author' do
+      expect(build(:timeline, author: nil)).not_to be_valid
+    end
+  end
 end
--- spec/models/timelines_teller_spec.rb
+++ spec/models/timelines_teller_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe TimelinesTeller, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:timelines_teller) { create(:timelines_teller) }
+
+  describe 'validations' do
+    it 'Create a valid timelines_teller' do
+      expect(timelines_teller).to be_valid
+    end
+
+    it 'Invalidwith same scope' do
+      expect(build(:timelines_teller, timeline_id: timelines_teller.timeline_id,
+                                      teller_id: timelines_teller.teller_id)).not_to be_valid
+    end
+  end
 end
