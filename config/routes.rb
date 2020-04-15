40caa5a8fc917f82bd47995f0b057ad20d8e350c
mfigand <m.figand@gmail.com>
2020-04-15 23:26:08 +0200

    Update index timeline route

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
       post 'authenticate', to: 'authentication#authenticate'
 
-      get 'users/:author_id/author_index', to: 'timelines#author_index', as: 'author_index'
-      get 'users/:protagonist_id/protagonist_index', to: 'timelines#protagonist_index', as: 'protagonist_index'
+      get 'users/:author_id/timelines/author_index', to: 'timelines#author_index', as: 'author_index'
+      get 'users/:protagonist_id/timelines/protagonist_index', to: 'timelines#protagonist_index', as: 'protagonist_index'
 
       resources :users do
         resources :timelines, only: %i[create show update destroy]
